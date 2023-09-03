a079ae8d0afe3576fefb86518dfac9c1d0de539c
IwuchukwuDivine <90557518+IwuchukwuDivine@users.noreply.github.com>
2023-09-03 09:37:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const searchButton = document.querySelector('.search-button');
+let inputName = document.querySelector('.city-name');
+
+
+const apiKey = "b57187c883c53425843dea1139bb6fe5";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+async function checkWeather(cityName) {
+  const response = await fetch(apiUrl + `q=${cityName}` + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector('.error-message').style.display = 'initial';
+  } else {
+    let data = await response.json();
+ 
+    document.querySelector('.temperature').innerHTML = Math.round(data.main.temp) + '°C';
+    document.querySelector('.city').innerHTML = cityName;
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + '%';
+    document.querySelector('.weather-image').src = 'images/' + data.weather[0].main + '.png';
+  
+    document.querySelector('.weather-open').style.display = "initial";
+    document.querySelector('.error-message').style.display = 'none';
+  }
+
+}
+searchButton.addEventListener('click', () => {
+  let cityName = inputName.value;
+  checkWeather(cityName);
+
+ 
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    let cityName = inputName.value;
+    checkWeather(cityName);
+    document.querySelector('.weather-open').style.display = "initial";
+  }
+})
